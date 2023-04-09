0d686a3ed6e224741505285c4575fd4442772478
SuryaaAulia <suryaaulia489@gmail.com>
2023-04-10 01:39:09 +0700

    menyelesailan program dengan runtime configuration

---
+++ tpmodul8_1302210084/tpmodul8_1302210084/CovidConfig.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul8_1302210084
+{
+    class CovidConfig
+    {
+
+        public string satuan_suhu { get; set; }
+        public int batas_hari_demam { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string pesan_diterima { get; set; }
+
+        public static CovidConfig LoadConfig()
+        {
+            CovidConfig config;
+            string configJson = File.ReadAllText(@"C:\Users\surya\Documents\KPL Praktikum\tpmodul8_1302210084\tpmodul8_1302210084\tpmodul8_1302210084\covid_config.json");
+            config = JsonConvert.DeserializeObject<CovidConfig>(configJson);
+            return config;
+        }
+
+        public static void UbahSatuan()
+        {
+            CovidConfig config = LoadConfig();
+            if (config.satuan_suhu == "celcius")
+            {
+                config.satuan_suhu = "fahrenheit";
+            }
+            else
+            {
+                config.satuan_suhu = "celcius";
+            }
+        }
+    }
+}
--- tpmodul8_1302210084/tpmodul8_1302210084/Program.cs
+++ tpmodul8_1302210084/tpmodul8_1302210084/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul8_1302210084;
+
+CovidConfig config = CovidConfig.LoadConfig();
+
+Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai" + config.satuan_suhu);
+string inputSuhu = Console.ReadLine();
+Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+string inputHari = Console.ReadLine();
+
+double suhu = double.Parse(inputSuhu);
+int hari = int.Parse(inputHari);
+
+if (config.satuan_suhu == "celcius")
+{
+    if (suhu >= 36.5 && suhu <= 37.5 && hari < config.batas_hari_demam)
+    {
+        Console.WriteLine(config.pesan_diterima);
+    }
+    else
+    {
+        Console.WriteLine(config.pesan_ditolak);
+    }
+}
+else
+{
+    double suhuF = (suhu * 9 / 5) + 32;
+    if (suhuF >= 97.7 && suhuF <= 99.5 && hari < config.batas_hari_demam)
+    {
+        Console.WriteLine(config.pesan_diterima);
+    }
+    else
+    {
+        Console.WriteLine(config.pesan_ditolak);
+    }
+}
